ac30708e74892a2f0a3be0d575dc145638dfa0c6
Kasra <kasra_ja1991@yahoo.com>
2022-08-26 13:47:14 -0400

    Update FindWinner.sol

--- FindWinner.sol
+++ FindWinner.sol
@@ -5,9 +5,9 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 contract Bet is VRFConsumerBaseV2{
+
     VRFCoordinatorV2Interface COORDINATOR;
-    // Your subscription ID.
-    uint64 s_subscriptionId;
+    uint64 s_subscriptionId; // Your subscription ID.
     // Goerli coordinator. For other networks,
     // see https://docs.chain.link/docs/vrf-contracts/#configurations
     address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D; //The address of the Chainlink VRF Coordinator contract.
@@ -22,22 +22,15 @@ contract Bet is VRFConsumerBaseV2{
     // and the processing of the callback request in the fulfillRandomWords()
     // function.
     uint32 callbackGasLimit = 100000; //The limit for how much gas to use for the callback request to your contract's fulfillRandomWords() function. It must be less than the maxGasLimit limit on the coordinator contract. In this example, the fulfillRandomWords() function stores two random values, which cost about 20,000 gas each, so a limit of 100,000 gas is sufficient. Adjust this value for larger requests depending on how your fulfillRandomWords() function processes and stores the received random values. If your callbackGasLimit is not sufficient, the callback will fail and your subscription is still charged for the work done to generate your requested random values.
-    // The default is 3, but you can set this higher.
-    uint16 requestConfirmations = 3; //How many confirmations the Chainlink node should wait before responding. The longer the node waits, the more secure the random value is. It must be greater than the minimumRequestBlockConfirmations limit on the coordinator contract.
-    // For this example, retrieve 2 random values in one request.
-    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+    uint16 requestConfirmations = 3; //The default is 3, but you can set this higher. How many confirmations the Chainlink node should wait before responding. The longer the node waits, the more secure the random value is. It must be greater than the minimumRequestBlockConfirmations limit on the coordinator contract.
     uint32 numWords =  1; //How many random values to request. If you can use several random values in a single callback, you can reduce the amount of gas that you spend per random value. The total cost of the callback request depends on how your fulfillRandomWords() function processes and stores the received random values, so adjust your callbackGasLimit accordingly.
     uint public s_randomRange;
     uint256 public s_requestId;
-    address s_owner;
+    // address s_owner;
 
 
-
-
-
-
-    address mainWallet = 0xEd79eA59df5d665802A13d9589675c52057a9A65;
-    uint costTicket = 0.000001 ether;
+    address mainWallet = 0xec001D225966Af2b4F0E7d5b79aa9455B2a1149a; //account 222
+    uint costTicket = 0.0000000000000001 ether; //100 Wei
     address owner;
     event LogBuyer(address accountBuyer);
 
@@ -49,7 +42,7 @@ contract Bet is VRFConsumerBaseV2{
         owner = msg.sender;
 
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-        s_owner = msg.sender;
+        // s_owner = msg.sender;
         s_subscriptionId = subscriptionId; //The subscription ID that this contract uses for funding requests.
     }
 
@@ -61,8 +54,7 @@ contract Bet is VRFConsumerBaseV2{
     }  
     function balance() public view returns(uint){
         return balances[msg.sender];
-    }
-
+    }  
 
     function buyTicket() public payable returns(address){
         require(costTicket <= balances[msg.sender], "Insufficient funds");
@@ -74,32 +66,25 @@ contract Bet is VRFConsumerBaseV2{
 
 
     modifier onlyOwner() {
-        require(msg.sender == s_owner);
+        require(msg.sender == owner);
         _;
     }
     // Assumes the subscription is funded sufficiently.
-    function requestRandomWords() external onlyOwner {   //  Takes your specified parameters and submits the request to the VRF coordinator contract.
-        // Will revert if subscription is not set and funded.
+    // Will revert if subscription is not set and funded.
+    //  Takes your specified parameters and submits the request to the VRF coordinator contract.
+    function requestRandomWords() external onlyOwner {   
         s_requestId = COORDINATOR.requestRandomWords(
-        keyHash,
-        s_subscriptionId,
-        requestConfirmations,
-        callbackGasLimit,
-        numWords
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
         );
     }
-
-    function fulfillRandomWords( //Receives random values and stores them with your contract.
-        uint256, /* requestId */
-        uint256[] memory randomWords
-    ) internal override {
+    //Receives random values and stores them with your contract.
+    function fulfillRandomWords(uint256, uint256[] memory randomWords) internal override {
         s_randomRange = (randomWords[0] % 50) + 1;
     }
-
-
-
-
-    
 
 
 }
